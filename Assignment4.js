90b35db562b5a645c14426ec6ec6039da71f6139
ShindeAkshay26 <115718193+ShindeAkshay26@users.noreply.github.com>
2024-02-27 10:23:29 +0530

    Add files via upload

--- Assignment4.js
+++ Assignment4.js
@@ -1,51 +1,32 @@
-// DataFetcher.js
+import React, { Component } from "react";
 
-import React, { Component } from 'react';
-
-class DataFetch extends Component {
-  constructor(props) {
+class Fetchapi extends Component {
+  constructor(props){
     super(props);
     this.state = {
-      data: null,
-      loading: true,
-      error: null
-    };
-  }
-
-  componentDidMount() {
-    fetch('https://api.example.com/data')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState({ data, loading: false });
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-  }
-
-  render() {
-    const { data, loading, error } = this.state;
-
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-
-    return (
-      <div>
-        <h2>DataFetcher Component</h2>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
-    );
-  }
+  data: {},
+   };
 }
 
-export default DataFetch;
+async componentDidMount(){
+  const url="https://v2.jokeapi.dev/joke/Programming";
+  const response=await fetch(url);
+  const data=await response.json();
+  this.setState({data});
+  console.log(this.state.data.setup);
+}
+render()
+{
+  return(
+    <center>
+      <div>
+        <h1>Jokes</h1>
+        {this.state.data.setup}<br></br>
+        {this.state.data.delivery}
+      </div>
+    </center>
+  )
+}
+}
+
+export default Fetchapi;
